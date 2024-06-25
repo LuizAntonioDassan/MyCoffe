441a8f32beffcc1d26ed69ac4aab20bba870ffdd
Matheus Biesdorf <122486659+matheusbiesdorf@users.noreply.github.com>
2024-06-25 18:47:13 -0400

    Ajuste no header

--- PHP-includes/header.php
+++ PHP-includes/header.php
@@ -8,33 +8,31 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produtos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Minha conta</a> <!-- Cliente -->
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Criar conta</a> 
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Entrar</a> <!-- Login -->
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Sair</a> <!-- Logout -->
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Painel de funcionário</a> <!-- Painel administrativo -->
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Carrinho</a>
-                    </li>
-
+                    <div class="links-navbar">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Minha conta</a> <!-- Painel do cliente -->
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sair</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Painel de funcionário</a>
+                        </li>
+                    </div>
+                    <div class="navbar-carrinho right">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Carrinho</a>
+                        </li>
+                    </div>
                 </ul>
             </div>
         </div>
