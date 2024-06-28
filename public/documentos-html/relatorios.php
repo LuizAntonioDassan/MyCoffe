3dac647cc72bbcd0932b00c02c5f43e2f1fb2070
Matheus Biesdorf <122486659+matheusbiesdorf@users.noreply.github.com>
2024-06-28 18:36:00 -0400

    Relatórios

---
+++ public/documentos-html/relatorios.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relatórios</title>
+</head>
+<body>
+    <?php include "navbar.php"?>
+
+    <main class="container my-5" style="max-width: 800px; margin: auto;">
+
+    <div class="card mb-4 p-3">
+      <h1 class="mb-4 text-center">Relatórios</h1>
+
+      
+      <div class="d-flex flex-column flex-md-row justify-content-center align-items-center mb-1">
+        <a href="#"><button class="btn mb-2 mx-2 mb-md-0">Relatório de vendas</button></a>
+        <a href="#"><button class="btn mb-2 mx-2 mb-md-0">Relatório de produtos</button></a>
+      </div>
+    </div>
+
+  </main>
+
+    <?php include "footer.php"?>
+</body>
+</html>
